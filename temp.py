3bd74b372304e27c8713604a6fe0eaef1b623ece
alexgonca <alexandre.reportagem@gmail.com>
2016-09-26 16:01:44 -0700

    create auxiliary agency tables

---
+++ temp.py
@@ -0,0 +1,15 @@
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+host = 'search-opencontractors-3r67ajhlmi4rd5r54ssz7pp4iu.us-west-1.es.amazonaws.com'
+awsauth = AWS4Auth('AKIAIU5VKBBJSCN3M72Q', 'BSO50gKFhx2vKAUtNjzKNOZeezEE4xffjsGt40Qk', 'us-west-1', 'es')
+
+es = Elasticsearch(
+    hosts=[{'host': host, 'port': 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
+
+print(es.info())
--- temp.sql
+++ temp.sql
@@ -1,8 +1,26 @@
-update contract
-set contractor_congressional_district = upper(contractor_congressional_district);
-update contract
-set contractor_congressional_district_redundant = upper(contractor_congressional_district_redundant);
-update contract
-set place_of_performance_congressional_district = upper(place_of_performance_congressional_district);
-update contract
-set place_of_performance_congressional_district_redundant = upper(place_of_performance_congressional_district_redundant);
+create table contracting_agency_top_vendors AS
+  select contracting_agency_id,
+    contractor_duns_number,
+    count(*) as contract_count,
+    sum (dollars_obligated) as total_amount
+  from contract
+  group by contracting_agency_id,
+    contractor_duns_number;
+
+create table funding_agency_top_vendors AS
+  select funding_agency_id,
+    contractor_duns_number,
+    count(*) as contract_count,
+    sum (dollars_obligated) as total_amount
+  from contract
+  group by funding_agency_id,
+    contractor_duns_number;
+
+create table contracting_agency_top_products_and_services AS
+  select contracting_agency_id,
+    product_or_service_code,
+    count(*) as contract_count,
+    sum (dollars_obligated) as total_amount
+  from contract
+  group by contracting_agency_id,
+    product_or_service_code;

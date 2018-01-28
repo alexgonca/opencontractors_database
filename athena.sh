2fbb92b6b95af3e26c09f71dc46a33b1eb79e5b9
Alexandre Goncalves <alexandre.reportagem@gmail.com>
2018-01-27 23:35:35 -0500

    add athena.sh

---
+++ athena.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# declare an array called array and define 3 vales
+array=(
+a76action
+active_schema
+)
+
+command = "\copy anything to program 'gzip > ./athena/anything.gz' with csv"
+for i in "${array[@]}"
+do
+    psql -h localhost -U postgres -d opencontractors -c "${command/anything/$i}"
+    s3cmd --continue-put put ./athena/deparment_name.gz s3://opencontractors/${i}/${i}.gz
+	echo $i
+done
